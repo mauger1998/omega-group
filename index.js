d27293607e066dfc1b4a389e4ebe8170aaf2d9bd
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-07-12 07:14:28 +0100

    Taking a break

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-// // gsap.utils.clamp(0, maxScroll, triggerValue)
+// gsap.utils.clamp(0, maxScroll, triggerValue)
 
-// // create the smooth scroller FIRST!
+// create the smooth scroller FIRST!
 
 //  let smoother = ScrollSmoother.create({
 //    smooth: 4,   // seconds it takes to "catch up" to native scroll position
-//    effects: true // look for data-speed and data-lag attributes on elements and animate accordingly
+//    effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
+//    smoothTouch:0.1,
 //  });
 
 
@@ -24,7 +25,7 @@
 //     ease: 
 //     Power4.
 //     easeInOut,
-//     stagger:0.5,
+//     stagger:0.3,
   
 //   });
 
