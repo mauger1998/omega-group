9c51751b994f3143909d8c64cc51afe3f103c3fe
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-08-16 15:14:39 +0100

    Most recent button updates

--- about-omega/index.js
+++ about-omega/index.js
@@ -199,3 +199,22 @@ menuClose.addEventListener("click", () => {
 
 
 
+
+
+
+// Accordian
+const items = document.querySelectorAll(".hiring-list-item button");
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (let item of items) {
+    item.setAttribute("aria-expanded", false);
+  }
+
+  if (itemToggle === "false") {
+    this.setAttribute("aria-expanded", true);
+  }
+}
